3fd14aa807aaf0d784e057f72be5a910457ea8e5
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-11-06 21:52:36 +0700

    Add files via upload

---
+++ PBO10119042Latihan39.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Muhammad Rifqu Abdillah
+ *  Nama : Muhammad Rifqu Abdillah
+ *  Kelas: Pbo-IF1
+ *  Nim : 10119042 
+ *  Deskripsi : Program ini berisi untuk menampilkan nilai Terbesar dan Terkecil
+ */
+public class PBO10119042Latihan39 {
+    
+    public static void main(String[] args) {
+       
+        NilaiTerbesarTerkecil nilaiMhs = new NilaiTerbesarTerkecil();
+        nilaiMhs.tampilNilaiMhs();
+    }
+    
+}
